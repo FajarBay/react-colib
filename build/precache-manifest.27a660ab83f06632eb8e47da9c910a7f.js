0d48f111c286a8eb83a6c18440052643eaeb154b
FajarBay <fajarbay@github.com>
2019-05-13 12:28:46 +0700

    gh

---
+++ build/precache-manifest.27a660ab83f06632eb8e47da9c910a7f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a984ee4809efcbec751a9bc1dfedd11",
+    "url": "/react-colid/index.html"
+  },
+  {
+    "revision": "f741d6dfff2727764242",
+    "url": "/react-colid/static/css/main.83bd8ac9.chunk.css"
+  },
+  {
+    "revision": "844f5e06a054850e9b68",
+    "url": "/react-colid/static/js/2.fc84c080.chunk.js"
+  },
+  {
+    "revision": "f741d6dfff2727764242",
+    "url": "/react-colid/static/js/main.e992c724.chunk.js"
+  },
+  {
+    "revision": "2a148b0b211b4fc94541",
+    "url": "/react-colid/static/js/runtime~main.5048a972.js"
+  }
+]);
